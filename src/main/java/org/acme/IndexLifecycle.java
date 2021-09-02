63ab07de61739db869a7d79be31fd0ba331e61ee
Mike Hepburn <eformat@gmail.com>
2021-09-02 14:38:38 +1000

    🦚 add index lifecycle create if not exists 🦚

---
+++ src/main/java/org/acme/IndexLifecycle.java
@@ -0,0 +1,31 @@
+package org.acme;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+
+@ApplicationScoped
+public class IndexLifecycle {
+
+    @Inject
+    IndexResource indexResource;
+
+    @ConfigProperty(name = "onstartup.create.index")
+    boolean onStart;
+
+    void onStart(@Observes StartupEvent ev) {
+        Response response = indexResource.count("engagements-read");
+        if (onStart && response.getStatus() != Response.Status.OK.getStatusCode()) {
+            indexResource.recreateIndex("engagements", "1", "1", false);
+        }
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        // empty
+    }
+}
